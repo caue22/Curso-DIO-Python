3e13ede3f283816f99be4fb57d061bac61388005
caue22 <cauegarcian@gmail.com>
2025-08-04 18:49:43 -0300

    Organizando repo

--- estrutura_condicionais.py
+++
@@ -1,8 +0,0 @@
-saldo = 2000.0
-saque = float(input("Informe o valor do saque: "))
-
-if saldo >= saque:
-    print("Saque realizado!")
-else:
-    print("Saldo insuficiente!")
-    
--- multiplas_linhas.py
+++
@@ -1,7 +0,0 @@
-nome = "Caue"
-
-print(f'''
-Olá meu nome é {nome},
-estou aprendendo python
-    '''
-    )
